41e6bd5a366b960cf3e15c7e7937e9060e788c16
dbryn24 <davebryangamer@gmail.com>
2024-11-07 21:32:27 +0800

    Exercise-13

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Client from "./components/Clients";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+
+const App = () => {
+  return (
+    <div className="body-wrap">
+      <Header />
+
+  <main className="site-content">
+    <Hero />
+    <Client />
+    <Features />
+    <Pricing />
+  </main>
+  <Footer />
+</div>
+
+  )
+}
+
+export default App;
